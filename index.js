cb6da1f3a5068f8a30a2c59eb054af785821e65a
Antony <antonygmurimi@gmail.com>
2024-04-02 21:31:27 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'mark';
+//function that accesses  global variable, and uppercases it.
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+    
+}
+//  upperCaseCustomerName();
+
+//declares a variable called bestCustomer in global scope and assigns it to be 'not bob'
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+   
+} 
+//overites best customer
+//    setBestCustomer();
+
+   function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+     
+ } 
+ 
+//  overwriteBestCustomer();
+ //least customer
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'john';
+     
+ } 
+ 
+   
+
+
+    
+    
+
+    
+    
